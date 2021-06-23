9d6bfdaba1c58a6af5a2e0f94193147aeac5adf7
SnowPanther37 <creepershowmem@gmail.com>
2021-06-23 15:37:40 +0300

    'wewe'

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,28 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    app: './index.js'
+  },
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+
+  module: {
+    rules: [{
+      test: /\.js$/, // include .js files
+      enforce: "pre", // preload the jshint loader
+      exclude: /node_modules/, // exclude any and all files in the node_modules folder
+      use: [{
+        loader: "jshint-loader",
+        // more options in the optional jshint object
+        options: {  // ⬅ formally jshint property
+          camelcase: true,
+          emitErrors: false,
+          failOnHint: false
+        }
+      }]
+    }]
   },
 };
