cbce18e16632102ab40eb6bcf09e00f835ffaf4d
SnowPanther37 <creepershowmem@gmail.com>
2021-06-24 10:13:04 +0300

    'wewe'

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,29 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: {
-    app: './index.js'
-  },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-
-  module: {
-    rules: [{
-      test: /\.js$/, // include .js files
-      enforce: "pre", // preload the jshint loader
-      exclude: /node_modules/, // exclude any and all files in the node_modules folder
-      use: [{
-        loader: "jshint-loader",
-        // more options in the optional jshint object
-        options: {  // ⬅ formally jshint property
-          camelcase: true,
-          emitErrors: false,
-          failOnHint: false
-        }
-      }]
-    }]
-  },
-  node: {
-    global: false,
-    __filename: false,
-    __dirname: false,
-  },
-};
+    mode: 'development',
+    entry: {
+        app: './src/index.js'
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist'
+    },
+    plugins: [
+        new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+        new HtmlWebpackPlugin({
+            title: 'Development'
+        })
+    ],
+    node: {
+      module: false,
+      net: false,
+      fs: false
+    }
+}
